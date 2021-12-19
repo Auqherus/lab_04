cfb3e5c872c9a1e39725e08e5ffcd6ab28b635aa
Auqherus <patrykromanowski@gmail.com>
2021-12-19 10:32:55 +0100

    zad1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+
+
+        System.out.println("To jest przyklad nr1");
+        System.out.printf("To jest przyklad nr2");
+        System.out.println();
+        System.out.print("To jest przyklad nr3");
+
+
     }
 }
