40a111c31b8aa50d9fed65e6d7305c86eb8bb209
Auqherus <patrykromanowski@gmail.com>
2022-01-03 21:07:37 +0100

    ćw 6

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.5 */
+        /* Tablice - cw_6 */
 
-        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-         * */
-
-
-        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+        /* zad1. stworzyć tablicę typu double 5-cio elementową,
+        przypisać do niej wartości i wyświetlić */
 
 
-        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        double tab [] = new double[5];
+
+        tab[0] = 0;
+        tab[1] = 1;
+        tab[2] = 2;
+        tab[3] = 3;
+        tab[4] = 4;
+
+        for (double tablica : tab) System.out.println(tablica);
+
+
 
 
     }
 }
-
