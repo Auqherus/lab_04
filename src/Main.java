21f76069ccf8715c68851995cc0968b60539b5e3
Auqherus <patrykromanowski@gmail.com>
2022-01-15 14:20:00 +0100

    ćw 2.1 a)

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,20 @@
-import javax.swing.*;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        /* cw.1 pętle while */
+        /* ćw.2 pętle for */
 
-        /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
-          Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
-          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
-        */
-
-
-        /* zad 2
-        a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
-          przeiterować tablicę po elementach i wyswietlić, użyć pętli while
-        b) jak wyżej tylko dla tablicy typu double[],
-        c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
-        */
-
-        /* zad.3
-        a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
-        b) a jak gdy damy false --> while(false)
-        */
+        /* zad.1 Napisz program który będzie iterował od 100 do 0,
+         *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
+         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
 
+        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
+         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
+         * za pomocą pętli forEach powiększone o 11 */
 
-        // zad 3 a)
-    /*    String tab [] = new String [5];
+         // zad 1 a) nr indeksu 60725
 
-        tab[0] = "Tadeusz";
-        tab[1] = "Adrian";
-        tab[2] = "Ignacy";
-        tab[3] = "Juliusz";
-        tab[4] = "Werter";
-        Arrays.sort(tab);
-        int i = 4;
-        while(true)
-        { System.out.println(tab[i]);i--;} // dzialanie zawsze prawdziwe*/
-        // zad 3 b)
-        String tab [] = new String [5];
-
-        tab[0] = "Tadeusz";
-        tab[1] = "Adrian";
-        tab[2] = "Ignacy";
-        tab[3] = "Juliusz";
-        tab[4] = "Werter";
-        Arrays.sort(tab);
-        int i = 4;
-        while(false)
-        { System.out.println(tab[i]);i--;} // dzialanie zawsze nieprawdziwe
-
-
+        for (int i = 0; i <= 100; i++) {if(i%5 == 0){System.out.println(i);}}
     }
 }
