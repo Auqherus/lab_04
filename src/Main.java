3ee605e21583b51fe193bb785fde9b49edf0d855
Auqherus <patrykromanowski@gmail.com>
2022-01-15 18:40:30 +0100

    ćw 5.1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        /* ćwiczenie 5 Metody */
 
-        /* cw.4  */
-
-        /* zad.1 napisać 3 pętle for i użyc:
-        a) w pierwszej pętli break
-        b) w drugiej pętli continue
-        c) w trzeciej pętli return
+        /* zad.1
+        a) stworzyć tablicę typu int[20],
+        b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 21,
+        c) napisać metodę, która wypisze otrzymane wartości,
+        d) wstawić wywołanie metody do metody main
         */
 
-        for (int i = 0; i <= 3; i++) {
-            System.out.println("Numer :"+" "+i);
-            break;
-        }
-        for (int i = 0; i <= 3; i++) {
-            System.out.println("Numer :"+" "+i);
-            continue;
-        }
-        for (int i = 0; i <= 3; i++) {
-            System.out.println("Numer :"+" "+i);
-            return;
-        }
+        /* zad.2
+        a) stworzyc tablicę typu String[6],
+        b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury
+            (użyć do tego pętli i klasy Scanner),
+        c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
+        */
 
+        /* zad.3
+        a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
+            wyświetla w konsoli string który ma być waszym imieniem,
+        b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
+            wasze imie i nazwisko,
+        c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
+            nazwisko i wiek (int), a następnie wszystko wyświetli,
+        */
+
+        /* zad.4
+        a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
+            coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
+        b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
+            którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
+        c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
+        */
+
+        int [] tab = new int[20];
+        for (int i = 0; i <tab.length ; i++) {tab[i] = i+21;System.out.println(tab[i]);}
     }
 }
