61eefc53a8dd401b19ec012778bcb51f05e60a18
Auqherus <patrykromanowski@gmail.com>
2022-01-15 18:11:06 +0100

    ćw 3.2

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,51 @@ public class Main {
          */
 
         //zad 1
-        Scanner TryMe = new Scanner(System.in);
+        /*Scanner TryMe = new Scanner(System.in);
         System.out.print("Podaj liczbe: " + " "); int a = TryMe.nextInt();
-        switch (a % 2) {case 0 -> System.out.println("Liczba parzysta!");case 1 -> System.out.println("Nieparzysta!");}
+        switch (a % 2) {case 0 -> System.out.println("Liczba parzysta!");case 1 -> System.out.println("Nieparzysta!");}*/
+
+        //zad 2
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0};
+        /* toCharArray() - metoda zamienia String na tablicę char[] */
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o':
+                    ++tablica[0];
+                    break;
+                case 'p':
+                    ++tablica[1];
+                    break;
+                case 'x':
+                    ++tablica[2];
+                    break;
+                case 'z':
+                    ++tablica[3];
+                    break;
+                case 'q':
+                    ++tablica[4];
+                    break;
+                case 'n':
+                    ++tablica[5];
+                    break;
+                case 'm':
+                    ++tablica[6];
+                    break;
+                case 'i':
+                    ++tablica[7];
+                    break;
+                case 't':
+                    ++tablica[8];
+                    break;
+                case 'j':
+                    ++tablica[9];
+                    break;
+            }
+        }
+        System.out.printf("\nLiczba występuje\no: %d - razy\nz: %d - razy\nj: %d - razy,\nx: %d - razy\nt: %d - razy\nn: %d - razy," +
+                        "\nm: %d - razy\ni: %d - razy\np: %d - razy\nq: %d - razy",
+                tablica[0], tablica[1], tablica[2],tablica[3], tablica[4], tablica[5],tablica[6], tablica[7], tablica[8],tablica[9]);
+
     }
 }
