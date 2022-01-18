b2160b088ce5d237ca40d3a20d262c203f7da14f
Auqherus <patrykromanowski@gmail.com>
2022-01-18 13:55:59 +0100

    ćw 3a)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-
-import student.Student;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,44 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /* ćw 3 klasy */
 
-        Student [] diary = new Student[3];
-        Student s1 = new Student("Endriu","John",21,true);
-        Student s2 = new Student("Arek","Wess",22,false);
-        Student s3 = new Student("Marcin","Kostowski",23,true);
+        /*  zad.1 - Najpierw należy uzupełnić kod w klasie Punkt.java,
+            a) stworzyć objekt punktA typu Punkt,
+            w konstruktorze zainicjować zmienną pX --> np. new Punkt(7), wykonać metodę punktA.toString();
+            b) następnie ponownie wywopać konstruktor inicjalizujący wszystkie pola z klasy
+            --> np. punkt = new Punkt(1, 2, 3);, wykonać metodę punktA.toString();
+            c) nadpisać wartości pól z objektu punktA za pomocą setterów i
+            ponownie wywołać metodę toString();
+            d) wypisać wartości pól (za pomocą getterów),
+            e) wywołać metody suma i róznica (obie wersje) z klasy punkt
+        */
 
-        diary[0] = s1;
-        diary[1] = s2;
-        diary[2] = s3;
-
-        /*for (int i = 0; i <diary.length; i++) {
-            System.out.println(diary[i].getInfo());
-        }*/
-        for (Student spr: diary) {
-
-            System.out.println(spr.getInfo());
-        }
-
-     /*
-        s1.getInfo();
-        s2.getInfo();
-        s3.getInfo();*/
-
-        /* ćw 2 klasy */
-        /* zad.1
-            a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
-            klasa ma się znajdować w osobnym pliku Student.java
-            b) stworzyć 3 objekty klasy student i wstawić je do tablicy
-            c) wyświetlić dane z tablicy, użyć pętli for lub forEach
-            d) dane powinny być estetycznie przedstawione
-         */
     }
 }
-class Tab {
 
-    public Tab(){
+class PunktA {
 
+    PunktA punkt = new PunktA(7);
+    private final int px;
+    public PunktA(int px){
+        this.px = px;
     }
 
+    public int getPx() {
+        return px;
+    }
+
+    public void metoda1(){
+        System.out.println(punkt.toString());
+    }
 
 }
