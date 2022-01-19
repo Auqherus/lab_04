887bd8bf155ac101409cf976f3fe3585a9267995
Auqherus <patrykromanowski@gmail.com>
2022-01-19 20:28:48 +0100

    ćw 1

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,46 @@
-import osoba.Osoba;
-import student.Student;
-import java.util.stream.IntStream;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-
     public static void main(String[] args) {
-        /* ćw 5 Dziedziczenie */
 
-        /* zad.1 Proszę wybrać dowolny temat budowania struktur klas np. samochody, dom, biblioteka, nauka, zabawki, kosmetyki, biuro podróży ... .
-                 Będziemy budować podobną strukturę kodu jak w przykładzie 5,
-            a) Proszę stworzyć układ klas dziedziczących po sobie,
-            b) proszę użyć pól i metod protected jak i private,
-            c) proszę stworzyć konstruktory, metody: toString(), settery i gettery,
-            d) proszę używać super do wywołania konstruktorów i metod z klas nadrzędnych
-            (można wygenerować je --> prawy przycisk myszki --> generate --> settery, gettery, ...),
-            e) w klasie Main proszę stworzyć objekty podobnie jak w przykładzie 5,
-            oraz stworzyć listę typu najbardziej generycznego np. List<Budynek> */
+        /* zad.1
+        a) Stwożyć interfejs Budynek,
+        b) interfejs powinien mieć metody:
+            - double powierzchnia(double pow);
+            - String adresBudynku(String adres);
+            - int liczbaOkien(int value);
+            - void liczbaMieszkancow(int value);
+            - void kolorDomu(KolorEnum kolor);
+        c) stworzyć enum KolorEnum z przykładowymi kolorami,
+        d) stworzyć jedną metodę z 2 parametrami wymyśloną przez siebie,
+        */
 
+        /* zad.2
+        a) napisać klasę Dom implementującą interfejs Budynek,
+        b) zaimplementować wszystkie metody,
+        c) stworzyć brakujące pola(prywatne), gettery i settery, napisać metodę toString
+        (gettery i settery najlepiej wygenerować: Code/Prawy myszy --> Generate --> getters/setters/toString)
+        d) stworzyć jedną dowolną metodę przez siebie
+        */
 
-        // Wykonujac zadania z cw 4 zrobilem w sumie przy okazji cw 5, wiec je daje do grupy
-
-        Osoba o1 = new Student("Geralt", "Wiedzminowski",38,234);
-
-        Student s1 = new Student("Adrian","Kwietniewski",20);
-        Student s2 = new Student("Marek","Strozik",14,22);
-        Student s3 = new Student("Karol","Slawski",17,21);
-        Student s4 = new Student("Wiktor","Krum",11,19);
-        Student s5 = new Student("Oliwier","Maskowski",15,23);
-
-        List<Student> list1 = new ArrayList<>(); // stworzenie tablicy
-        list1.add(s1);
-        list1.add(s2);
-        list1.add(s3);
-        list1.add(s4);
-        list1.add(s5);
-
-        for (Student nowy : list1) {System.out.println(nowy.getInfo());} //pierwsza lista
-
-        System.out.println("<<======================PO ZMIANIE STUDENTOW========================>>");
-        s1.setImie("Tomek");s1.setNazwisko("Marowski");s1.setIndex(33);s1.setAge(24);
-        s2.setImie("Kevin");s1.setNazwisko("Ackoski");s2.setIndex(4);s2.setAge(0);
-        s3.setImie("Bartek");s1.setNazwisko("Rajtak");s3.setIndex(8);s3.setAge(19);
-        s4.setImie("Filip");s1.setNazwisko("Wazowski");s4.setIndex(42);s4.setAge(18);
-        s5.setImie("Eugieniusz");s1.setNazwisko("Cieskalowski");s5.setIndex(2);s5.setAge(32);
-
-        for (Student nowy : list1) {System.out.println(nowy.getInfo());} //lista po setterach
-
-
-
+        /* zad.3
+        a) w klasie Main stworzyć 2 objekty typu Dom deklarując pierwszy objekt Typem Dom,
+        drugi objekt typem Budynek,
+        b) wykorzystać wszystkie metody do implementacji pól
+        c) wyświetlić w konsoli metodę toString (na obu objektach) opisującą zachowanie klasy Dom
+        */
     }
+}
+
+interface Budynek {
+    double powierzchniaBudynku(double pow);
+    String adresBudynku(String adres);
+    int liczbaOkien(int value);
+    void liczbaMieszkancow(int value);
+    void kolorDomu(KolorEnum kolor);
+    int oknaBudynku(int oknaMale, int oknaDuze);
+
+    enum KolorEnum{
+        Niebieski, Czerwony, Czarny, Zielony
+    }
+
+
 }
